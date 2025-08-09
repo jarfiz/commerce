886b153b4975a679eeff59502202f6be9628e429
Jaenudin Firdaus <jaenudinfirdaus2004@gmail.com>
2025-08-09 11:41:20 +0700

    feat: create Checkout component with cart display and form for user details

---
+++ src/components/pages/chekcout.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Image from "next/image";
+
+const Checkout = () => {
+  return (
+    <div className="container mx-auto mt-10 px-4">
+      <h1 className="text-2xl font-medium">Your cart</h1>
+      <div className="mt-10 grid grid-cols-[1fr_440px] gap-12">
+        <div className="flex flex-col space-y-2 p-4">
+          <div className="flex outline">
+            <Image
+              src="https://cdn.dummyjson.com/product-images/beauty/red-lipstick/thumbnail.webp"
+              alt="images"
+              width={80}
+              height={80}
+            />
+            <div className="flex flex-1 items-center justify-between px-6">
+              <div>
+                <h1>Product 1</h1>
+                <p className="text-xl font-medium">$99</p>
+              </div>
+              <div className="flex items-center space-x-4">
+                <div className="space-x-2">
+                  <Button size="sm" variant="outline">
+                    +
+                  </Button>
+                  <span>1</span>
+                  <Button size="sm" variant="outline">
+                    -
+                  </Button>
+                </div>
+                <Button size="sm">Remove</Button>
+              </div>
+            </div>
+          </div>
+          <div className="mt-6 flex items-center justify-between">
+            <p>Total</p>
+            <Button>Remove All Product</Button>
+          </div>
+        </div>
+
+        {/* form */}
+        <div className="space-y-4 px-10 py-5 outline">
+          <div className="space-y-1.5">
+            <Label htmlFor="first-name text-sm">First Name</Label>
+            <Input placeholder="First Name" />
+          </div>
+          <div className="space-y-1.5">
+            <Label htmlFor="first-name text-sm">Last Name</Label>
+            <Input placeholder="Last Name" />
+          </div>
+          <div className="space-y-1.5">
+            <Label htmlFor="first-name text-sm">Email</Label>
+            <Input placeholder="Email" />
+          </div>
+          <div className="space-y-1.5">
+            <Label htmlFor="first-name text-sm">Phone</Label>
+            <Input placeholder="Phone" />
+          </div>
+          <Button className="w-full">Checkout</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
