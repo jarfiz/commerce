cf9d32fb14c888db940e63828b47c9fa89c0e174
Jaenudin Firdaus <jaenudinfirdaus2004@gmail.com>
2025-08-09 11:50:57 +0700

    fix: update key prop in Products mapping to use product.id for better performance

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -17,8 +17,8 @@ const Home = () => {
     <div className="mt-20 mb-20 space-y-20">
       <h1 className="text-center text-4xl font-bold">Our Products</h1>
       <div className="container mx-auto grid grid-cols-4 gap-6 px-4">
-        {products.map((product) => (
-          <Products key={product.id} product={product} />
+        {products.map((product, index) => (
+          <Products key={index} product={product} />
         ))}
       </div>
     </div>
