17336e8591ec91b44cd49624a8eb0d16cabcb2f5
Jaenudin Firdaus <jaenudinfirdaus2004@gmail.com>
2025-08-10 14:26:24 +0700

    feat: enhance CheckoutForm with validation schema and improved UI components

--- src/components/products/checkout-form.tsx
+++ src/components/products/checkout-form.tsx
@@ -1,28 +1,117 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+// schema
+const formSchema = z.object({
+  firstName: z.string().min(4, {
+    error: "First name must be at least 4 characters",
+  }),
+  lastName: z.string().min(4, {
+    error: "Last name must be at least 4 characters",
+  }),
+  email: z.email(),
+  phone: z.string(),
+});
 
 const CheckoutForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
-    <form className="h-fit space-y-4 px-10 py-5 outline">
-      <div className="space-y-1.5">
-        <Label htmlFor="first-name text-sm">First Name</Label>
-        <Input placeholder="First Name" />
-      </div>
-      <div className="space-y-1.5">
-        <Label htmlFor="first-name text-sm">Last Name</Label>
-        <Input placeholder="Last Name" />
-      </div>
-      <div className="space-y-1.5">
-        <Label htmlFor="first-name text-sm">Email</Label>
-        <Input placeholder="Email" />
-      </div>
-      <div className="space-y-1.5">
-        <Label htmlFor="first-name text-sm">Phone</Label>
-        <Input placeholder="Phone" />
-      </div>
-      <Button className="w-full">Checkout</Button>
-    </form>
+    <div className="h-fit rounded-md p-4 outline">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+          {/* first name */}
+          <FormField
+            control={form.control}
+            name="firstName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>First Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription>Enter your first name</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* last name */}
+          <FormField
+            control={form.control}
+            name="lastName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Last Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription>Enter your last name</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* email */}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription>Enter your email address</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* phone */}
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription>Enter your phone number</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="mt-2 w-full">
+            Pay now
+          </Button>
+        </form>
+      </Form>
+    </div>
   );
 };
 
